07b095972ce2785756a8a3621d0b9aca570d5429
JonasChai <jonas.chairutdinovas@gmail.com>
2023-12-26 13:44:12 +0200

    added DB connection

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
 import "dotenv/config";
 
 const app = express();
 
+mongoose
+    .connect(process.env.DB_CONNECTION)
+    .then(() => console.log("DB CONNECTION ESTABLISHED!"))
+    .catch((err) => {
+        console.log(err);
+    });
+
 app.listen(process.env.PORT, () => {
     console.log(`App running on port: ${process.env.PORT}`);
-})
+});
